27f5b56ea1b63b00630bf98af1b40ac6c477abe1
Ermakov Pavel <pde0177776@gmail.com>
2022-05-24 15:40:22 +0400

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.galery__slider').slick({
+        prevArrow: '.galery__btnl',
+        nextArrow: '.galery__btnr',
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+
+    });
+})
